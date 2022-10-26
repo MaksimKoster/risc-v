e140c5cc8d3a1f6f6acc2ac587baddfe874f0f24
Maksim Kostritsya <m.kostritsya@yandex.ru>
2022-10-26 20:55:53 +0300

    CS fixed

--- test.c
+++ test.c
@@ -29,52 +29,51 @@ int dst_array[ARR_SIZE];
 #define PORT 16
 #define IMMEDIATE 108
 
-int main(void)
-{
-	int *src = src_array + SRC_OFF;
-	int *dst = dst_array + DST_OFF;
-	int wrong = 0;
-	int i, err;
-	struct dma_addr addr;
+#define PORT 16
+#define IMMEDIATE 108
 
-	for (i = 0; i < ARR_SIZE; i++)
-		dst_array[i] = 0;
+int main(void) {
+    int * src = src_array + SRC_OFF;
+    int * dst = dst_array + DST_OFF;
+    int wrong = 0;
+    int i, err;
+    struct dma_addr addr;
 
-	addr.addr = 0;
-	addr.port = PORT;
-	dma_bind_addr(&addr);
+    for (i = 0; i < ARR_SIZE; i++)
+        dst_array[i] = 0;
 
-	dma_gather_put(&addr, dst, src, COPY_SIZE * sizeof(int), 0, 1);
-	dma_fence();
-	err = dma_send_error();
-	if (err)
-		return 0x40 | err;
+    addr.addr = 0;
+    addr.port = PORT;
+    dma_bind_addr( & addr);
 
-	dma_read_src_addr(&addr);
-	if (addr.port != PORT)
-		return 0x30 | err;
+    dma_gather_put( & addr, dst, src, COPY_SIZE * sizeof(int), 0, 1);
+    dma_fence();
+    err = dma_send_error();
+    if (err)
+        return 0x40 | err;
 
-	for (i = 0; i < DST_OFF; i++) {
-		if (dst_array[i] != 0)
-			wrong = 1;
-	}
+    dma_read_src_addr( & addr);
+    if (addr.port != PORT)
+        return 0x30 | err;
 
-	for (i = 0; i < COPY_SIZE; i++) {
-		if (dst[i] != src[i])
-			wrong = 1;
-	}
+    for (i = 0; i < DST_OFF; i++) {
+        if (dst_array[i] != 0)
+            wrong = 1;
+    }
 
-	for (i = DST_OFF + COPY_SIZE; i < ARR_SIZE; i++) {
-		if (dst_array[i] != 0)
-			wrong = 1;
-	}
+    for (i = 0; i < COPY_SIZE; i++) {
+        if (dst[i] != src[i])
+            wrong = 1;
+    }
+
+    for (i = DST_OFF + COPY_SIZE; i < ARR_SIZE; i++) {
+        if (dst_array[i] != 0)
+            wrong = 1;
+    }
 
     if (wrong) {
         print_uart0("memory filled uncorrectly");
-    }
-    else print_uart0("memory filled correctly");
+    } else print_uart0("memory filled correctly");
 
-
-	return 0;
+    return 0;
 }
-
